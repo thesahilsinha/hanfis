d418bf43e75cf97283f2fde2604717c5271c4db7
Sahil Sinha <99786329+thesahilsinha@users.noreply.github.com>
2026-06-29 16:49:06 +0530

    brand images

--- components/home/BrandSection.tsx
+++ components/home/BrandSection.tsx
@@ -3,17 +3,17 @@ import Link from 'next/link'
 
 const brands = [
   { name: 'Apple', href: '/products?brand=Apple', logo: 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg' },
-  { name: 'Samsung', href: '/products?brand=Samsung', logo: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg' },
-  { name: 'OnePlus', href: '/products?brand=OnePlus', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/OnePlus_logo.svg/512px-OnePlus_logo.svg.png' },
+  { name: 'Samsung', href: '/products?brand=Samsung', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Samsung_Orig_Wordmark_BLACK_RGB.png/960px-Samsung_Orig_Wordmark_BLACK_RGB.png' },
+  { name: 'OnePlus', href: '/products?brand=OnePlus', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Logo_entreprise_OnePlus.png/960px-Logo_entreprise_OnePlus.png' },
   { name: 'Xiaomi', href: '/products?brand=Xiaomi', logo: 'https://upload.wikimedia.org/wikipedia/commons/2/29/Xiaomi_logo.svg' },
-  { name: 'Vivo', href: '/products?brand=Vivo', logo: 'https://upload.wikimedia.org/wikipedia/commons/1/17/Vivo_logo.png' },
-  { name: 'OPPO', href: '/products?brand=OPPO', logo: 'https://upload.wikimedia.org/wikipedia/commons/2/2e/OPPO_LOGO_2019.svg' },
-  { name: 'Realme', href: '/products?brand=Realme', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Realme_logo.svg/512px-Realme_logo.svg.png' },
-  { name: 'Motorola', href: '/products?brand=Motorola', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Motorola_logo_2014.svg/512px-Motorola_logo_2014.svg.png' },
-  { name: 'Nokia', href: '/products?brand=Nokia', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Nokia_wordmark.svg/512px-Nokia_wordmark.svg.png' },
-  { name: 'Google', href: '/products?brand=Google', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/512px-Google_2015_logo.svg.png' },
-  { name: 'iQOO', href: '/products?brand=iQOO', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/IQoo_logo.png/512px-IQoo_logo.png' },
-  { name: 'Nothing', href: '/products?brand=Nothing', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Nothing_Technology_logo.svg/512px-Nothing_Technology_logo.svg.png' },
+  { name: 'Vivo', href: '/products?brand=Vivo', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Vivo_mobile_logo.png/960px-Vivo_mobile_logo.png' },
+  { name: 'OPPO', href: '/products?brand=OPPO', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/OPPO_LOGO.jpg/960px-OPPO_LOGO.jpg' },
+  { name: 'Realme', href: '/products?brand=Realme', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Realme-realme-_logo_box-RGB-01.svg/960px-Realme-realme-_logo_box-RGB-01.svg.png' },
+  { name: 'Motorola', href: '/products?brand=Motorola', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Motorola-logo-black-and-white.png/960px-Motorola-logo-black-and-white.png' },
+  { name: 'Nokia', href: '/products?brand=Nokia', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Nokia_2023.svg/960px-Nokia_2023.svg.png' },
+  { name: 'Google', href: '/products?brand=Google', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/960px-Google_2015_logo.svg.png' },
+  { name: 'iQOO', href: '/products?brand=iQOO', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/IQOO_logo.svg/960px-IQOO_logo.svg.png' },
+  { name: 'Nothing', href: '/products?brand=Nothing', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Nothing_Tech.jpg/960px-Nothing_Tech.jpg' },
 ]
 
 export default function BrandSection() {
